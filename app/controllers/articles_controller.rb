cefed419a5d349e4af2f0bd714f153087921c54f
Jaime <jaime.cruz.hostalot@gmail.com>
2020-02-15 21:51:09 +0100

    Adding new pages preparing linking and functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,5 +8,17 @@ class ArticlesController < ApplicationController
   @article = Article.find(params[:id])
   end
 
+  def create
+    @article = Article.new
+
+
+    @article.save
+    format.html {redirect_to @article, notice: 'Article was successfully created.'}
+  end
+
+  def update
+    #empty, future usage.
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get 'articles/index'
   root controller: :articles, action: :index
   resources :articles, only: [:index, :show]
+  resources :articles, only: [:create]
+
+  
 end
